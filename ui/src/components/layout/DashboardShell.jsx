52075e8a69cc843417376a7eb873634962a17e74
Shukuru <shukurukamanzi26@gmail.com>
2026-08-02 19:47:40 +0300

    Fixed the SideBar and TopBar to have Fixed position allowing them to not Scroll too

--- ui/src/components/layout/DashboardShell.jsx
+++ ui/src/components/layout/DashboardShell.jsx
@@ -6,14 +6,16 @@ import LogoWatermark from "../ui/LogoWatermark";
 
 export default function DashboardShell() {
   return (
-    <div className="mesh-bg min-h-screen flex relative overflow-hidden">
+    <div className="mesh-bg h-[100dvh] w-full flex overflow-hidden relative">
       <LogoWatermark />
       <Sidebar />
-      <div className="flex-1 flex flex-col px-4 lg:px-6 pt-4 pb-24 lg:pb-8 max-w-[1600px] mx-auto w-full relative z-10">
-        <Topbar />
-        <main className="flex-1">
-          <Outlet />
-        </main>
+      <div className="flex-1 h-full overflow-y-auto relative z-10">
+        <div className="flex flex-col min-h-full px-4 lg:px-6 pt-4 pb-24 lg:pb-8 max-w-[1600px] mx-auto w-full">
+          <Topbar />
+          <main className="flex-1 flex flex-col">
+            <Outlet />
+          </main>
+        </div>
       </div>
       <TabBar />
     </div>
--- ui/src/layouts/DashboardShell.jsx
+++ ui/src/layouts/DashboardShell.jsx
@@ -4,13 +4,15 @@ import Topbar from "./Topbar";
 
 export default function DashboardShell() {
   return (
-    <div className="mesh-bg min-h-screen flex">
+    <div className="mesh-bg h-[100dvh] w-full flex overflow-hidden">
       <Sidebar />
-      <div className="flex-1 flex flex-col px-4 lg:px-6 pt-4 pb-8 max-w-[1600px] mx-auto w-full">
-        <Topbar />
-        <main className="flex-1">
-          <Outlet />
-        </main>
+      <div className="flex-1 h-full overflow-y-auto relative">
+        <div className="flex flex-col min-h-full px-4 lg:px-6 pt-4 pb-8 max-w-[1600px] mx-auto w-full">
+          <Topbar />
+          <main className="flex-1 flex flex-col">
+            <Outlet />
+          </main>
+        </div>
       </div>
     </div>
   );
